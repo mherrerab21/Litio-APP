0e4debbbe392dd410582f0d2b6cdb3b3eeb8d223
mherrerab21 <166243667+mherrerab21@users.noreply.github.com>
2024-04-06 21:30:59 -0300

    Update streamlit_dashboard.py

--- streamlit_dashboard.py
+++ streamlit_dashboard.py
@@ -106,6 +106,7 @@ st.write(variacion)
 st.title("Precios de Contrato a lo largo del Tiempo")
 selected_prices = st.multiselect("Seleccionar Precio de Contrato:", df_market.columns)
 if selected_prices:
-    st.line_chart(df_market[selected_prices], use_container_width=True)
+    chart = st.line_chart(df_market[selected_prices], use_container_width=True)
+    chart.pyplot().invert_xaxis()  # Invertir el eje x del gráfico
 else:
     st.warning("Por favor selecciona al menos un precio de contrato.")
