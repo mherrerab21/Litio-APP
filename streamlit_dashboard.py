e834ee968c4920d8a66de721893f4c3306868553
mherrerab21 <166243667+mherrerab21@users.noreply.github.com>
2024-04-07 22:42:06 -0400

    Update streamlit_dashboard.py

--- streamlit_dashboard.py
+++ streamlit_dashboard.py
@@ -16,7 +16,7 @@ st.sidebar.title("Dashboard de Precios de Litio y Derivados")
 # Opciones del sidebar
 option = st.sidebar.selectbox(
     'Seleccione una opción',
-    ('Precios de Contrato', 'Contract Data')
+    ('Precios de Contrato', 'Contrato Futuro 2407')  # Cambio de 'Contract Data' a 'Contrato Futuro 2407'
 )
 
 # Cambiar el color de fondo de la página a un verde militar
@@ -65,82 +65,83 @@ except Exception as e:  # Handle different exceptions
 
 # Read Data from Excel
 nombre_archivo_excel = 'futuros litio.xlsx'
-df_market = pd.read_excel(nombre_archivo_excel, sheet_name='Market')
 
-# Data Cleaning and Transformation (corrected renaming)
-df_market.rename(columns={'Conversion en Notas': 'Fecha'}, inplace=True)
-df_market.set_index('Fecha', inplace=True)
-
-# Column Name Cleaning
-nombres_columnas_originales = ['Industrial\n Grade', 'SMM\n LC Idx', 'Battery \nGrade', 'Lithium \nHydroxide BG',
-                              'Lithium Hydroxide Idx', 'Lithium Hydroxide \nBG Fine', 'Lithium Carbonate\n CIF China',
-                              'Lithium Hydroxide\n CIF China', 'Lithium \nHexafluorophosphate', 'Lithium \nFluoride BG',
-                              'Lithium\n Hydroxide IG', 'Spodumene \nConcentrate IDX\nCIF China', 'Spodumene Domestic\n China 5%',
-                              'Spodumene \nDomestic China 4%', 'Spodumene \nDometic China 3%', 'Spodumene\n1,2%',
-                              'Spodumene\n2%', 'Spodumene\n3%', 'Lepidolite \n1,5%', 'Lepidolite \n2%', 'Montebrasite \n6%',
-                              'Montebrasite \n7%', 'AUS Spodumene 6% Spot cif China', 'BRL Spodumene 6% Spot CIF China']
-
-nombres_columnas_actualizados = [nombre.replace('/', '').replace('\n', '') for nombre in nombres_columnas_originales]
-
-df_market.columns = nombres_columnas_actualizados
-
-# Column Removal
-columnas_a_eliminar = ['Lithium Hydroxide Idx', 'SMM LC Idx', 'Lithium Hydroxide BG Fine', 'Lithium Hexafluorophosphate',
-                      'Lithium Fluoride BG', 'Spodumene Domestic China 4%', 'Spodumene Dometic China 3%',
-                      'Spodumene1,2%', 'Spodumene2%', 'Spodumene3%', 'Lepidolite 1,5%', 'Lepidolite 2%',
-                      'Montebrasite 6%', 'Montebrasite 7%']
-
-df_market = df_market.drop(columns=columnas_a_eliminar)
-
-# Convertir el índice a objetos de fecha y luego formatear las fechas
-df_market.index = pd.to_datetime(df_market.index).strftime('%d/%m/%y')
-
-# Función para obtener tipo de cambio de USD a CNY desde Yahoo Finance
-def obtener_tipo_cambio(ticker):
-    try:
-        data_yahoo = yf.download(ticker, start=start_date, end=end_date)
-        if not data_yahoo.empty:  # Check if data is empty
-            return round(data_yahoo['Close'].iloc[0], 2)  # Access closing price and round to 2 decimal places
-        else:
-            st.warning("No data downloaded for {} in the specified date range.".format(ticker))
-            return None
-    except Exception as e:  # Handle different exceptions
-        st.error("Error downloading data for {}: {}".format(ticker, e))
-        return None
-
-# Obtener tipo de cambio de USD a CNY desde Yahoo Finance
-tipo_cambio_USD_CNY = obtener_tipo_cambio('USDCNY=X')
-
-# Definir factor de conversión de KG a metric tons
-factor_conversion_KG_to_MT = 1000  # 1 KG = 0.001 metric tons
-
-if tipo_cambio_USD_CNY is not None:
-    # Operaciones a realizar en las columnas especificadas
-    columnas_operaciones = {
-        'Industrial Grade': lambda x: x / tipo_cambio_USD_CNY,  # Mantener el resultado como número flotante
-        'Battery Grade': lambda x: x / tipo_cambio_USD_CNY,
-        'Lithium Hydroxide BG': lambda x: x / tipo_cambio_USD_CNY,
-        'Lithium Hydroxide IG': lambda x: x / tipo_cambio_USD_CNY,
-        'Spodumene Domestic China 5%': lambda x: (x * 7.5) + 3750,  # Mantener el resultado como número flotante
-        'AUS Spodumene 6% Spot cif China': lambda x: (x * 7.5) + 3750,  # Mantener el resultado como número flotante
-        'BRL Spodumene 6% Spot CIF China': lambda x: (x * 7.5) + 3750,  # Mantener el resultado como número flotante
-        'Lithium Carbonate CIF China': lambda x: x / tipo_cambio_USD_CNY * factor_conversion_KG_to_MT,  # Mantener el resultado como número flotante
-        'Lithium Hydroxide CIF China': lambda x: x / tipo_cambio_USD_CNY * factor_conversion_KG_to_MT  # Mantener el resultado como número flotante
-    }
-
-    # Aplicar operaciones a las columnas correspondientes
-    for columna, operacion in columnas_operaciones.items():
-        if columna in df_market.columns:
-            df_market[columna] = df_market[columna].apply(operacion)
-
-# Reemplazar los valores NaN por 0
-df_market = df_market.fillna(0)
-
-# Calcular las variaciones porcentuales entre el último y penúltimo dato para cada columna
-variacion_porcentual = (df_market.diff().iloc[-1] / df_market.iloc[-2]) * 100
-
-# Mostrar DataFrame actualizado
 if option == 'Precios de Contrato':
+    df_market = pd.read_excel(nombre_archivo_excel, sheet_name='Market')
+
+    # Data Cleaning and Transformation (corrected renaming)
+    df_market.rename(columns={'Conversion en Notas': 'Fecha'}, inplace=True)
+    df_market.set_index('Fecha', inplace=True)
+
+    # Column Name Cleaning
+    nombres_columnas_originales = ['Industrial\n Grade', 'SMM\n LC Idx', 'Battery \nGrade', 'Lithium \nHydroxide BG',
+                                  'Lithium Hydroxide Idx', 'Lithium Hydroxide \nBG Fine', 'Lithium Carbonate\n CIF China',
+                                  'Lithium Hydroxide\n CIF China', 'Lithium \nHexafluorophosphate', 'Lithium \nFluoride BG',
+                                  'Lithium\n Hydroxide IG', 'Spodumene \nConcentrate IDX\nCIF China', 'Spodumene Domestic\n China 5%',
+                                  'Spodumene \nDomestic China 4%', 'Spodumene \nDometic China 3%', 'Spodumene\n1,2%',
+                                  'Spodumene\n2%', 'Spodumene\n3%', 'Lepidolite \n1,5%', 'Lepidolite \n2%', 'Montebrasite \n6%',
+                                  'Montebrasite \n7%', 'AUS Spodumene 6% Spot cif China', 'BRL Spodumene 6% Spot CIF China']
+
+    nombres_columnas_actualizados = [nombre.replace('/', '').replace('\n', '') for nombre in nombres_columnas_originales]
+
+    df_market.columns = nombres_columnas_actualizados
+
+    # Column Removal
+    columnas_a_eliminar = ['Lithium Hydroxide Idx', 'SMM LC Idx', 'Lithium Hydroxide BG Fine', 'Lithium Hexafluorophosphate',
+                          'Lithium Fluoride BG', 'Spodumene Domestic China 4%', 'Spodumene Dometic China 3%',
+                          'Spodumene1,2%', 'Spodumene2%', 'Spodumene3%', 'Lepidolite 1,5%', 'Lepidolite 2%',
+                          'Montebrasite 6%', 'Montebrasite 7%']
+
+    df_market = df_market.drop(columns=columnas_a_eliminar)
+
+    # Convertir el índice a objetos de fecha y luego formatear las fechas
+    df_market.index = pd.to_datetime(df_market.index).strftime('%d/%m/%y')
+
+    # Función para obtener tipo de cambio de USD a CNY desde Yahoo Finance
+    def obtener_tipo_cambio(ticker):
+        try:
+            data_yahoo = yf.download(ticker, start=start_date, end=end_date)
+            if not data_yahoo.empty:  # Check if data is empty
+                return round(data_yahoo['Close'].iloc[0], 2)  # Access closing price and round to 2 decimal places
+            else:
+                st.warning("No data downloaded for {} in the specified date range.".format(ticker))
+                return None
+        except Exception as e:  # Handle different exceptions
+            st.error("Error downloading data for {}: {}".format(ticker, e))
+            return None
+
+    # Obtener tipo de cambio de USD a CNY desde Yahoo Finance
+    tipo_cambio_USD_CNY = obtener_tipo_cambio('USDCNY=X')
+
+    # Definir factor de conversión de KG a metric tons
+    factor_conversion_KG_to_MT = 1000  # 1 KG = 0.001 metric tons
+
+    if tipo_cambio_USD_CNY is not None:
+        # Operaciones a realizar en las columnas especificadas
+        columnas_operaciones = {
+            'Industrial Grade': lambda x: x / tipo_cambio_USD_CNY,  # Mantener el resultado como número flotante
+            'Battery Grade': lambda x: x / tipo_cambio_USD_CNY,
+            'Lithium Hydroxide BG': lambda x: x / tipo_cambio_USD_CNY,
+            'Lithium Hydroxide IG': lambda x: x / tipo_cambio_USD_CNY,
+            'Spodumene Domestic China 5%': lambda x: (x * 7.5) + 3750,  # Mantener el resultado como número flotante
+            'AUS Spodumene 6% Spot cif China': lambda x: (x * 7.5) + 3750,  # Mantener el resultado como número flotante
+            'BRL Spodumene 6% Spot CIF China': lambda x: (x * 7.5) + 3750,  # Mantener el resultado como número flotante
+            'Lithium Carbonate CIF China': lambda x: x / tipo_cambio_USD_CNY * factor_conversion_KG_to_MT,  # Mantener el resultado como número flotante
+            'Lithium Hydroxide CIF China': lambda x: x / tipo_cambio_USD_CNY * factor_conversion_KG_to_MT  # Mantener el resultado como número flotante
+        }
+
+        # Aplicar operaciones a las columnas correspondientes
+        for columna, operacion in columnas_operaciones.items():
+            if columna in df_market.columns:
+                df_market[columna] = df_market[columna].apply(operacion)
+
+    # Reemplazar los valores NaN por 0
+    df_market = df_market.fillna(0)
+
+    # Calcular las variaciones porcentuales entre el último y penúltimo dato para cada columna
+    variacion_porcentual = (df_market.diff().iloc[-1] / df_market.iloc[-2]) * 100
+
+    # Mostrar DataFrame actualizado
     st.markdown("## Precios de Contrato:")
     st.dataframe(df_market)
 
@@ -161,9 +162,8 @@ if option == 'Precios de Contrato':
     else:
         st.warning("Por favor selecciona al menos un precio de contrato.")
 
-elif option == 'Contract Data':
+elif option == 'Contrato Futuro 2407':  # Cambio de 'Contract Data' a 'Contrato Futuro 2407'
     # Read Data from Excel
-    nombre_archivo_excel = 'futuros litio.xlsx'
     df_lc2407 = pd.read_excel(nombre_archivo_excel, sheet_name='Contrato 2407')
 
     # Data Cleaning and Transformation (corrected renaming)
@@ -175,11 +175,11 @@ elif option == 'Contract Data':
     df_lc2407['O.I %'] = df_lc2407['O.I %'] * 100
 
     # Mostrar DataFrame actualizado
-    st.markdown("## Contract Data:")
+    st.markdown("## Contrato Futuro 2407:")  # Cambio de 'Contract Data' a 'Contrato Futuro 2407'
     st.dataframe(df_lc2407)
 
     # Graficar los datos de las columnas 'Latest' y 'Volume'
     fig_lc2407 = px.line(df_lc2407, x=df_lc2407.index, y=['Latest', 'Volume'], labels={'Date': 'Fecha', 'value': 'Valor', 'variable': 'Variable'})
-    fig_lc2407.update_layout(title="Datos de Contrato 2407", xaxis_title="Fecha", yaxis_title="Valor", legend_title="Variable", width=1600, height=600)
+    fig_lc2407.update_layout(title="Datos de Contrato Futuro 2407", xaxis_title="Fecha", yaxis_title="Valor", legend_title="Variable", width=1600, height=600)  # Cambio de 'Contract Data' a 'Contrato Futuro 2407'
     fig_lc2407.update_traces(hovertemplate='%{x}<br>%{y}')
     st.plotly_chart(fig_lc2407, use_container_width=False, config={'displayModeBar': True, 'scrollZoom': False})
